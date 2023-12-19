c7e872631e683899c382ece423f6776903cc588f
Tom Ellis <s3965617@student.rmit.edu.au>
2023-12-19 20:00:10 +1100

    Pick the logos based on teams competing

--- Assets/GCInfo.cs
+++ Assets/GCInfo.cs
@@ -9,12 +9,13 @@ using System.Net.Sockets;
 using System.Text;
 using Data;
 using System.IO;
+using System.Threading;
 public class GCInfo : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text _title;
     // Use a wildcard address for the target
-    IPAddress targetIpAddress = IPAddress.Parse("192.168.40.157");
+    IPAddress targetIpAddress = IPAddress.Any;
 
     // Ports for different types of messages
     int monitorPort = 3636;
@@ -24,115 +25,144 @@ public class GCInfo : MonoBehaviour
     // Start is called before the first frame update
 
     // Create UDP clients for each port
-    UdpClient monitorRequestClient = new UdpClient();
-    UdpClient controlClient = new UdpClient(3838);
-    UdpClient forwardedStatusClient = new UdpClient();
+    UdpClient monitorRequestClient;
+    UdpClient controlClient;
+    UdpClient forwardedStatusClient;
+
     void Start()
     {
         Debug.Log("Start");
-        // try
-        // {
-        // First send initial monitor reques
+        controlClient = new UdpClient(controlPort);
+        IPEndPoint remoteEndPoint = new IPEndPoint(targetIpAddress, controlPort);
+        byte[] receivedData = controlClient.Receive(ref remoteEndPoint);
+        Debug.Log(remoteEndPoint.Address.ToString());
+        monitorRequestClient = new UdpClient();
         byte[] packet = Encoding.ASCII.GetBytes("RGTr\0");
-        monitorRequestClient.Send(packet, packet.Length, new IPEndPoint(targetIpAddress, monitorPort));
+        monitorRequestClient.Send(packet, packet.Length, new IPEndPoint(remoteEndPoint.Address, monitorPort));
         _title.text = "Monitor request sent.";
         monitorRequestClient.Close();
-
-
-        // // Listen for packets
-        string data = ReceiveMessages(controlClient, targetIpAddress, 3636, "RGTr", "Regular Control");
-        _title.text = data;
-        Debug.Log("hmmmm " + data);
-        // }
-        // catch (Exception ex)
-        // {
-        //     Debug.LogError($"An error occurred: {ex.Message}");
-        // }
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log("Update");/
-
-        // // Listen for packets
-        string data = ReceiveMessages(controlClient, targetIpAddress, 3636, "RGTr", "Regular Control");
-        if (data != "FAIL")
+        Debug.Log("Update");
+        if (controlClient != null)
         {
-            _title.text = data;
-
+            string data = ReceiveMessages(controlClient, targetIpAddress, controlPort, "RGTr", "Regular Control");
+            if (data != "FAIL")
+            {
+                _title.text = data;
+            }
         }
-        // Debug.Log("hmmmm " + data);
-
-
-
     }
 
     void OnDestroy()
     {
-        controlClient.Close();
-        monitorRequestClient.Close();
-        forwardedStatusClient.Close();
-        // Add similar lines for other UdpClient instances if needed
+        if (controlClient != null)
+        {
+            controlClient.Close();
+        }
+
+        if (monitorRequestClient != null)
+        {
+            monitorRequestClient.Close();
+        }
+
+        if (forwardedStatusClient != null)
+        {
+            forwardedStatusClient.Close();
+        }
     }
 
 
 
     static string ReceiveMessages(UdpClient controlClient, IPAddress targetIpAddress, int targetPort, string headerMagic, string messageType)
     {
-        // Debug.Log("targetIpAddress:" + targetIpAddress + " targetPort:" + targetPort);
         IPEndPoint remoteEndPoint = new IPEndPoint(targetIpAddress, targetPort);
-        Debug.Log(remoteEndPoint.ToString());
-        byte[] receivedData = controlClient.Receive(ref remoteEndPoint);
-        // Debug.Log("WOWOW");
-        // Debug.Log(receivedData.ToString());
-        string receivedHeaderMagic = Encoding.ASCII.GetString(receivedData, 0, 4);
-        string receivedMessage = Encoding.ASCII.GetString(receivedData);
-        int byteArrayLength = receivedMessage.Length;
-        string decodedMessage = Encoding.ASCII.GetString(receivedData, 5, byteArrayLength - 5);
-        // Console.WriteLine($"{receivedHeaderMagic} Received: {receivedMessage} Extracted: {decodedMessage}");
-
-        GameControlData data = new GameControlData();
-
-        using (MemoryStream memoryStream = new MemoryStream(receivedData))
+        try
         {
-            // // Create a BinaryReader to read from the MemoryStream
-            // using (BinaryReader binaryReader = new BinaryReader(memoryStream))
-            // {
-            if (data.FromByteArray(memoryStream))
+            byte[] receivedData = controlClient.Receive(ref remoteEndPoint);
+            string receivedHeaderMagic = Encoding.ASCII.GetString(receivedData, 0, 4);
+            string receivedMessage = Encoding.ASCII.GetString(receivedData);
+            int byteArrayLength = receivedMessage.Length;
+            string decodedMessage = Encoding.ASCII.GetString(receivedData, 5, byteArrayLength - 5);
+            Debug.Log($"{receivedHeaderMagic} Received: {receivedMessage} Extracted: {decodedMessage}");
+
+            GameControlData data = new GameControlData();
+
+            using (MemoryStream memoryStream = new MemoryStream(receivedData))
             {
-                // Console.WriteLine("Looks like we got data!");
-
-                if (data.isTrueData)
+                if (data.FromByteArray(memoryStream))
                 {
-                    StringBuilder output = new StringBuilder();
-                    // timestampOfLastTrueGameControlData = System.currentTimeMillis();
-                    // output.AppendLine("Data seems to be true data!");
-                    output.AppendLine(data.ToString());
-                    output.AppendLine("#############################################");
-                    string teamName0 = GetTeamName(data.team[0].teamNumber);
-                    output.AppendLine($"Team Number: {data.team[0].teamNumber} Name: {teamName0}");
-                    output.AppendLine($"Goals: {data.team[0].score}");
-                    List<int> playerNums0 = new List<int> { 1, 2, 3, 4, 5 };
-                    output.AppendLine(data.team[0].PlayersToSring(playerNums0));
-                    output.AppendLine("#############################################");
-                    string teamName1 = GetTeamName(data.team[1].teamNumber);
-                    output.AppendLine($"Team Number: {data.team[1].teamNumber} Name: {teamName1}");
-                    output.AppendLine($"Goals: {data.team[1].score}");
-                    List<int> playerNums1 = new List<int> { 1, 2, 3, 4, 5 };
-                    output.AppendLine(data.team[1].PlayersToSring(playerNums1));
+                    if (data.isTrueData)
+                    {
 
-                    // Console.WriteLine(data.team[0].ToString());
-                    // Console.WriteLine(data.team[1].ToString());
-                    output.AppendLine("#############################################");
-                    return (output.ToString());
+                        // Upate the Team Logos
+                        GameObject firstTeamLogoObject = GameObject.Find("FirstTeamLogo");
+                        if (firstTeamLogoObject != null){
+                            LogoController firstTeamLogoController = firstTeamLogoObject.GetComponent<LogoController>();
+
+                            if (firstTeamLogoController != null) {
+                                firstTeamLogoController.SetTeamNumber(data.team[0].teamNumber);
+                            }
+                            else {
+                                Debug.LogError("LogoController component not found on the GameObject.");
+                            }
+                        }
+                        else {
+                            Debug.LogError("GameObject with the specified name not found.");
+                        }
+
+                        // Upate the Team Logos
+                        GameObject secondTeamLogoObject = GameObject.Find("SecondTeamLogo");
+                        if (secondTeamLogoObject != null){
+                            LogoController secondTeamLogoController = secondTeamLogoObject.GetComponent<LogoController>();
+
+                            if (secondTeamLogoController != null) {
+                                secondTeamLogoController.SetTeamNumber(data.team[1].teamNumber);
+                            }
+                            else {
+                                Debug.LogError("LogoController component not found on the GameObject.");
+                            }
+                        }
+                        else {
+                            Debug.LogError("GameObject with the specified name not found.");
+                        }
+
+                        StringBuilder output = new StringBuilder();
+                        output.AppendLine(data.ToString());
+                        output.AppendLine("#############################################");
+                        string teamName0 = GetTeamName(data.team[0].teamNumber);
+                        output.AppendLine($"Team Number: {data.team[0].teamNumber} Name: {teamName0}");
+                        output.AppendLine($"Goals: {data.team[0].score}");
+                        List<int> playerNums0 = new List<int> { 1, 2, 3, 4, 5 };
+                        output.AppendLine(data.team[0].PlayersToSring(playerNums0));
+                        output.AppendLine("#############################################");
+                        string teamName1 = GetTeamName(data.team[1].teamNumber);
+                        output.AppendLine($"Team Number: {data.team[1].teamNumber} Name: {teamName1}");
+                        output.AppendLine($"Goals: {data.team[1].score}");
+                        List<int> playerNums1 = new List<int> { 1, 2, 3, 4, 5 };
+                        output.AppendLine(data.team[1].PlayersToSring(playerNums1));
+                        output.AppendLine("#############################################");
+                        Debug.Log(output.ToString());
+                        return (output.ToString());
+                    }
                 }
+                return "FAIL";
             }
-            return "FAIL";
-            // }
-            // }
+        }
+        catch (Exception err)
+        {
+            Debug.Log(err.ToString());
+            return "Exception Occured";
         }
     }
+    
+
+    
 
 
     static string GetTeamName(byte teamNumber)
@@ -141,9 +171,6 @@ public class GCInfo : MonoBehaviour
 
         string[] teamNames = Teams.GetNames(true);
 
-        // foreach (string s in teamNames) {
-        //     Console.WriteLine(s);
-        // }
         if (teamNumber != null)
         {
             if (teamNumber < teamNames.Length && teamNames[teamNumber] != null)
